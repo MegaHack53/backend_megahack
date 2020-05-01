ef17d00adf0fd53f069990f53a75d58b0c2ec9ec
chseki <GERU\christian.seki@ger-nt-dev-076.geru.local>
2020-05-01 19:54:24 -0300

    chore: add transactions routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import './database/mongo';
+import routes from './routes';
 
 const app = express();
-
 const PORT = process.env.PORT || 3333;
+
+app.use(routes);
+app.use(express.json());
 
 app.get('/', (req,res) => res.json({message: 'hello megahack !'}))
 
