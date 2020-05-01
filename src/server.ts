e547c767bf55354238d2c0574ef546fa780594d5
faradayseki <chrisyuri_19@hotmail.com>
2020-05-01 16:55:04 -0300

    chore: add mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import './database/mongo';
 
 const app = express();
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(3333,() => console.log(`listen on port ${PORT}`))
-
 app.get('/', (req,res) => res.json({message: 'hello megahack !'}))
+
+app.listen(3333,() => console.log(`listen on port ${PORT}`))
