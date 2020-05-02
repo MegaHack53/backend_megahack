baed239499725511896b672e60ac33f1e540f5b7
iamseki <GERU\christian.seki@ger-nt-dev-076.geru.local>
2020-05-01 22:54:30 -0300

    chore: add transaction post/list

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import transactionRouter from './transaction.routes'
 
 const routes = Router();
 
+routes.get('/', (req,res) => res.json({message: 'hello megahack !'}))
+
 routes.use('/transactions', transactionRouter);
 
 export default routes;
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,22 +1,27 @@
 import { Router } from 'express';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
 
-transactionRouter.get('/', (request,response) => {
+transactionRouter.get('/', async (request,response) => {
   try {
-    return response.json({message: "transaction route"})
+    const transactions = await Transaction.find();
+
+    return response.json(transactions);
   }
   catch(err){
     return response.json({error: err.message});
   }
 })
 
-transactionRouter.post('/',(request,response) => {
+transactionRouter.post('/',async (request,response) => {
   try {
+    const transaction = Transaction.create(request.body);
 
-  }
+    return response.json(transaction);
+  } 
   catch(err){
-
+    return response.status(400).json({message: err.message})
   }
 })
 
